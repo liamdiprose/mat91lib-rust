7d21ecff0e5c48ff93263e48edbee1446007024c
liam <ldi30@uclive.ac.nz>
2018-03-26 01:12:23 +1300

    copies bindgen output to lib.rs file

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
+use std::fs::File;
+use std::io::{Read,Write};
+use std::vec::Vec;
 
 fn main() {
     //println!("cargo:root=mat91lib/sam4s");
@@ -17,4 +20,20 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Could not write bindings");
+
+    let mut lib_file = File::create("src/lib.rs").expect("Could not create src/lib file");
+    let mut prelude = File::open("src/prelude.rs").expect("Could not open prelude file");
+    let mut bindings = File::open(out_path.join("bindings.rs")).expect("Could not open bindings.rs");
+
+    let mut prelude_contents = Vec::with_capacity(200);
+    let mut bindings_content = Vec::with_capacity(5000);
+
+    prelude.read_to_end(&mut prelude_contents).expect("Could not read prelude.rs file");
+    lib_file.write_all(&prelude_contents);
+
+
+    bindings.read_to_end(&mut bindings_content).expect("Could not read bindings.rs file");
+    lib_file.write_all(&bindings_content).expect("Could not write bindings to src/lib.rs");
+
+
 }
--- wrapper.h
+++ wrapper.h
@@ -5,5 +5,6 @@
 #define __SAM4S__
 #define __SAM4S8B__
 #include "mat91lib.h"
+#include "pio.h"
 #include "pwm/pwm.h"
 #include "adc/adc.h"
